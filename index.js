13421793b93b8192ef487e648e3ab737306cd937
enlighten96gm <gm.enlightenby@gmail.com>
2022-03-19 17:33:07 +0300

    feat: start reserch

---
+++ index.js
@@ -0,0 +1,25 @@
+const Web3 = require('web3')
+const url = 'https://mainnet.infura.io/v3/49e219950b3f4b688c56f2f5bff154d7'
+const address = '0xDA9dfA130Df4dE4673b89022EE50ff26f6EA73Cf'
+
+//   Здесь мы смотрели балансы кошельков и регали свой!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// const web3 = new Web3(url)
+// const balance = web3.eth.getBalance(address, (err, bal) => bal)
+// ;
+// balance.then(res => {
+//     const balance = res
+//     const ethBalance = web3.utils.fromWei(balance, 'ether')
+//     // console.log(balance);
+//     // console.log(ethBalance);
+// })
+// const nyAcc = web3.eth.accounts.create()
+// console.log(nyAcc);
+//   Здесь мы смотрели балансы кошельков и регали свой!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//  Коннектим ганаж для получения работоспособных кошелков с эфиром !!!!!!!!!!!!!!!!!!!!!!!!!!
+// const web3 = new Web3('http://127.0.0.1:7545')
+// web3.eth.getBalance('0x94D3FF3caB18B3ad6b2410e842A95511a43e3710', (err, wei) => {
+//     const balance = web3.utils.fromWei(wei, 'ether')
+//     console.log(balance);
+// })
+//  Коннектим ганаж для получения работоспособных кошелков с эфиром !!!!!!!!!!!!!!!!!!!!!!!!!!
